869befe0624f67805586d969fecc647c5e5ebe9a
LucasDLeandro <ldlprodev@gmail.com>
2026-03-31 17:52:35 -0300

    Removendo arquivos sensíveis do rastreamento e atualizando urls

--- notificacoes/urls.py
+++ notificacoes/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', TemplateView.as_view(template_name='messages/base_message.html'), name='notificacoes'),
     path('api/contatos/criarContato/', adm_contato_view.api_criar_contato, name='api_criar_contato'),
     path('api/contatos/editarContato/<int:id_contato>/', adm_contato_view.api_editar_contato, name='api_editar_contato')
+    
 ]
