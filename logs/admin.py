7c9792ebd07475ee64f822f38156832b3a552b16
LucasDLeandro <ldlprodev@gmail.com>
2026-07-28 17:51:29 -0300

    Auto commit by Agent: Registra model SystemLog no admin e atualiza requirements.txt

--- logs/admin.py
+++ logs/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import SystemLog
 
-# Register your models here.
+@admin.register(SystemLog)
+class SystemLogAdmin(admin.ModelAdmin):
+    list_display = ('user', 'action', 'timestamp')
+    search_fields = ('user__username', 'action', 'details')
+    list_filter = ('action', 'timestamp')
